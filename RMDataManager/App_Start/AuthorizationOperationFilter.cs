c41d4113c9234ac94bbd73ca718d357a6c0a928a
aliae2425 <noam.carmi80@gmail.com>
2024-08-11 17:07:12 +0200

    AuthorisationFilter

---
+++ RMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace RMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            
+        }
+    }
+}
