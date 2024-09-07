a22423bcf68376ba25ce5ab761d75394aa3021c8
aliae2425 <noam.carmi80@gmail.com>
2024-09-07 18:41:43 +0200

    start early refactor

--- RMData/DBO/Tables/Inventory.sql
+++ RMData/DBO/Tables/Inventory.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[ProductID] INT NOT NULL,
 	[Quantity] INT NOT NULL Default 1,
-	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY ([ProductID]) REFERENCES Product([id]),
 )
--- RMData/DBO/Tables/SaleDetail.sql
+++ RMData/DBO/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
 	[ProductID] INT NOT NULL,
 	[Quantity] INT NOT NULL Default 1,
 	[PurchasePrice] MONEY NOT NULL,
-	[Tax] MONEY NOT NULL Default 0
+	[Tax] MONEY NOT NULL Default 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleID) REFERENCES Sale([ID]), 
+    CONSTRAINT [FK_SaleDetail_ToProducts] FOREIGN KEY (ProductID) REFERENCES [Product]([Id])
 )
