c4f0e19afbe92d86590cc0f29ce7a4c33017931f
aliae2425 <noam.carmi80@gmail.com>
2024-09-13 23:02:12 +0200

    create models

---
+++ RMDataManager/Controllers/SaleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    public class SaleController : ApiController
+    {
+        //POST: Sale - Create a new sale
+        public void Post([FromBody] SaleModel sale)
+        {
+        }
+    }
+}
